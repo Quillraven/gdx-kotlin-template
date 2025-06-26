303987308854cce69afa83347e0850fc31bab203
Quillraven <quillraven@gmail.com>
2025-06-26 21:54:06 +0200

    add snapshot repos and inc foojay resolver convention to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,18 @@ dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
         mavenCentral()
+        // LibGDX Snapshots
+        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
+        // Fleks Snapshots
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+        // LibKTX Snapshots
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
     }
 }
 
 plugins {
     // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 include(":core")
