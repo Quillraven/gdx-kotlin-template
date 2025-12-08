c292d98a79c82d25bba8bfdb1030478b9898d683
Quillraven <quillraven@gmail.com>
2025-12-08 16:53:51 +0700

    add construo plugin

--- lwjgl3/build.gradle.kts
+++ lwjgl3/build.gradle.kts
@@ -1,6 +1,9 @@
+import io.github.fourlastor.construo.Target
+
 plugins {
     buildsrc.convention.`kotlin-jvm`
     application
+    alias(libs.plugins.construoGradlePlugin)
 }
 
 dependencies {
@@ -34,6 +37,23 @@ tasks {
 
         manifest {
             attributes["Main-Class"] = application.mainClass
+        }
+    }
+}
+
+construo {
+    name.set(application.applicationName)
+    // human-readable name, used for example in the `.app` name for macOS
+    humanName.set(application.applicationName)
+
+    targets {
+        create<Target.Linux>("linuxX64") {
+            architecture.set(Target.Architecture.X86_64)
+            jdkUrl.set("https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.1%2B8/OpenJDK25U-jdk_x64_linux_hotspot_25.0.1_8.tar.gz")
+        }
+        create<Target.Windows>("winX64") {
+            architecture.set(Target.Architecture.X86_64)
+            jdkUrl.set("https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.1%2B8/OpenJDK25U-jdk_x64_windows_hotspot_25.0.1_8.zip")
         }
     }
 }
