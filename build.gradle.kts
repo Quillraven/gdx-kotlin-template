95cbecd123808454bb6fae2047e21234fbb8cee5
Quillraven <Quillraven@users.noreply.github.com>
2026-08-13 05:13:32 +0000

    deploy: c5e17be73092ddbda22f40d31083f8329052fb05

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    // Load the Kotlin plugin once in the common parent project so that subprojects
-    // applying it via the kotlin-jvm convention plugin share the same classloader.
-    alias(libs.plugins.kotlinJvm) apply false
-}
