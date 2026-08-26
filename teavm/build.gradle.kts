1236d54bdb4bc6967766d264c607e40e2f814951
Quillraven <quillraven@gmail.com>
2026-08-26 07:10:15 +0200

    update to gdx-teavm 1.6.2 and gradle 9.7.1

--- teavm/build.gradle.kts
+++ teavm/build.gradle.kts
@@ -13,25 +13,36 @@ gdxTeaVM {
     assets(rootProject.file("assets"))
     reflection("com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator")
 
-    js {
+    webDefaults {
         mainClass.set("io.github.TeaVMLauncherKt")
-        htmlTitle.set("GdxGame JS")
+        htmlTitle.set("GdxGame TeaVM")
         htmlWidth.set(1280)
         htmlHeight.set(720)
         serverPort.set(8080)
-        obfuscated.set(false)
     }
 
-    wasm {
-        mainClass.set("io.github.TeaVMLauncherKt")
-        htmlTitle.set("GdxGame Wasm")
-        htmlWidth.set(1280)
-        htmlHeight.set(720)
-        serverPort.set(8080)
+    // use JS during development since it is easier to test and debug.
+    // Also, the devServer is enabled for js below. You can also enable it for wasm, if you like.
+    js {
         obfuscated.set(false)
+
+        devServer {
+            enabled.set(true)
+            autoBuild.set(true) // detect changed sources/assets and automatically rebuild teavm stuff
+            autoReload.set(true) // automatically refresh the browser after a successful build
+        }
+    }
+
+    // use wasm for your final release as it has better performance usually
+    wasm {
+        obfuscated.set(true)
 
         // wasm generation sometimes needs more memory than Gradle's default 512 MiB daemon heap
         outOfProcess = true
         processMemory = 1024
+
+        devServer {
+            enabled.set(false)
+        }
     }
 }
